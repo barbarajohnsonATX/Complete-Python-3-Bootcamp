ebecdff74179c364f8b33b8ca510f1ec3f965322
BARBARA JOHNSON <barbarajohnson@BARBARAs-MacBook-Pro.local>
2021-03-03 21:00:18 -0600

    Done

---
+++ pylint_example/simple.py
@@ -0,0 +1,15 @@
+'''
+A very simple script
+'''
+
+def myfunc():
+    '''
+    A simple function
+    To run pylint on terminal: pylint simple.py -r y
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
